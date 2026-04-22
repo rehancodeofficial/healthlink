1fb4fd26d356d7fe759429c32f589c071ed97ea4
rehancodeofficial <163642322+rehancodeofficial@users.noreply.github.com>
2026-04-22 23:41:58 +0500

    Update frontend code

---
+++ frontend-repo-temp/src/main.jsx
@@ -0,0 +1,14 @@
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { ThemeProvider } from "./context/ThemeContext.jsx";
+
+import { SocketProvider } from "./context/SocketContext.jsx";
+
+createRoot(document.getElementById("root")).render(
+  <SocketProvider>
+    <ThemeProvider>
+      <App />
+    </ThemeProvider>
+  </SocketProvider>
+);
