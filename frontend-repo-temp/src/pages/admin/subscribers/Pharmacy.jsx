637168c1354061cd7481be7061196cd14c92f0b1
rehancodeofficial <163642322+rehancodeofficial@users.noreply.github.com>
2026-04-24 12:48:22 +0500

    Update frontend code

---
+++ frontend-repo-temp/src/pages/admin/subscribers/Pharmacy.jsx
@@ -0,0 +1,5 @@
+// FILE: src/pages/admin/subscribers/Pharmacy.jsx
+import SubscribersList from "../../shared/SubscribersList";
+export default function AdminSubscribedPharmacy() {
+  return <SubscribersList title="Subscribed Pharmacy" role="ADMIN" filterRole="PHARMACY" />;
+}
