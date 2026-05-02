1dcb50bd65b6f298b08ac363fbae0129a4b668cc
rehancodeofficial <163642322+rehancodeofficial@users.noreply.github.com>
2026-05-02 08:32:36 +0500

    Update frontend code

---
+++ frontend-repo-temp/src/pages/support/subscribers/Patients.jsx
@@ -0,0 +1,7 @@
+// FILE: src/pages/support/subscribers/Patients.jsx
+import SubscribersList from "../../shared/SubscribersList";
+export default function SupportSubscribedPatients() {
+  return <SubscribersList title="Subscribed Patients" role="SUPPORT" filterRole="PATIENT" />;
+}
+
+
