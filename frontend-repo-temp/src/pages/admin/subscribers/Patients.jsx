a7715999dea4ef04dc82a59a0e2a27f26947cc7e
rehancodeofficial <163642322+rehancodeofficial@users.noreply.github.com>
2026-05-18 04:52:16 +0500

    Update from frontend edits

--- frontend-repo-temp/src/pages/admin/subscribers/Patients.jsx
+++
@@ -1,5 +0,0 @@
-// FILE: src/pages/admin/subscribers/Patients.jsx
-import SubscribersList from "../../shared/SubscribersList";
-export default function AdminSubscribedPatients() {
-  return <SubscribersList title="Subscribed Patients" role="ADMIN" filterRole="PATIENT" />;
-}
