242ddf4ad9004f18bbf51bd7167c50c940fb5f82
rehancodeofficial <163642322+rehancodeofficial@users.noreply.github.com>
2026-05-25 23:05:40 +0500

    Update from frontend edits

--- frontend-repo-temp/src/pages/superadmin/subscribers/Doctor.jsx
+++
@@ -1,5 +0,0 @@
-// FILE: src/pages/superadmin/subscribers/Doctors.jsx
-import SubscribersList from "../../shared/SubscribersList";
-export default function SuperSubscribedDoctors() {
-  return <SubscribersList title="Subscribed Doctors" role="SUPERADMIN" filterRole="DOCTOR" />;
-}
