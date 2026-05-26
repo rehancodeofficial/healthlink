70451903d59c8e1fcf5443275c0f241345d58dc5
rehancodeofficial <163642322+rehancodeofficial@users.noreply.github.com>
2026-05-26 14:02:35 +0500

    Update from frontend edits

--- frontend-repo-temp/src/pages/support/subscribers/Pharmacy.jsx
+++
@@ -1,5 +0,0 @@
-// FILE: src/pages/support/subscribers/Pharmacy.jsx
-import SubscribersList from "../../shared/SubscribersList";
-export default function SupportSubscribedPharmacy() {
-  return <SubscribersList title="Subscribed Pharmacy" role="SUPPORT" filterRole="PHARMACY" />;
-}
