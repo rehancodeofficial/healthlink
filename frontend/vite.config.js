58b0317c988cf1dc883cc01d7469cffd7867a43f
wajihacodeofficial <wajihacodeofficial@gmail.com>
2026-07-19 11:46:24 +0500

    refactor: move frontend configuration and dependencies into the frontend directory

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,9 +1,9 @@
-// vite.config.js
 import path from "path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  root: __dirname,
   plugins: [react()],
   resolve: {
     alias: { "@": path.resolve(__dirname, "src") },
